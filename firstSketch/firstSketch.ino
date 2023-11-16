162017fd671137ad1123b46399e4cf4274d6e800
ishika2603 <ishtulsian@gmail.com>
2023-11-16 14:32:09 -0500

    testing led blink

---
+++ firstSketch/firstSketch.ino
@@ -0,0 +1,13 @@
+
+void setup() {
+    pinMode(13, OUTPUT);
+}
+
+void loop() {
+    // digitalWrite(13, HIGH);
+    // delay(0.008);
+    // digitalWrite(13, LOW);
+    // delay(0.09);
+    exit(0);
+
+}
